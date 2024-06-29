dc51668e052c7c2d72da7a84c0e851bbdb131a3a
Muhammad Hafidh Rosyadi <mhafidhrosyadi@gmail.com>
2024-06-29 22:23:55 +0700

    feat: add creategroup, createchat

--- chat.py
+++ chat.py
@@ -6,6 +6,8 @@ import logging
 import socket
 import threading
 from queue import Queue
+from datetime import datetime
+
 
 class Chat:
 	def __init__(self):
@@ -46,23 +48,23 @@ class Chat:
 		# 	port
 		# 	users
 		#   auth
-		self.realms["127.0.0.2"] = {
-				"port": 8889,
-				"users": [
-					"hmd",
-					"hfd"
-				],
-				"auth": "secret2"
-			}
-		self.realms["127.0.0.3"] = {
-				"port": 8889,
-				"users": [
-					"apt",
-					"hq",
-					"rzn"
-				],
-				"auth": "secret3"
-			}
+		# self.realms["127.0.0.2"] = {
+		# 		"port": 8889,
+		# 		"users": [
+		# 			"hmd",
+		# 			"hfd"
+		# 		],
+		# 		"auth": "secret2"
+		# 	}
+		# self.realms["127.0.0.3"] = {
+		# 		"port": 8889,
+		# 		"users": [
+		# 			"apt",
+		# 			"hq",
+		# 			"rzn"
+		# 		],
+		# 		"auth": "secret3"
+		# 	}
 		
   
 	def proses(self,data):
@@ -92,7 +94,32 @@ class Chat:
 				logging.warning("SYNC: addUserRealm {} {} {}" . format(auth, ipRealm, username))
 				result = self.add_user_realm(auth, ipRealm, username)
 				return result
-			
+			elif(command == 'addRealmChat'):
+				chat_id = j[1].strip()
+				username = j[2].strip()
+				logging.warning("SYNC: addRealmChat {} {} {}" . format(chat_id, chat_dict, username))
+				result = self.add_realm_chats(chat_id, username)
+				return result
+			elif(command == 'changeSelfChat'):
+				chat_id = j[1].strip()
+				chat_dict = ' '.join(j[2:]).strip()
+				logging.warning("SYNC: changeSelfChat {} {}" . format(chat_id, chat_dict))
+			elif (command == 'createGroup'):#type, group_name, members banyak
+				tokenid = j[1].strip()
+				type= j[2].strip()
+				group_name = j[3].strip()
+				password = j[4].strip()
+				logging.warning("CREATE_GROUP: createGroup {} {} {}" . format(type, group_name, password))
+				result = self.create_chat(tokenid, type, group_name, password = password)
+				return result
+			elif(command == 'createChat'): #type, member
+				tokenid = j[1].strip()
+				type = j[2].strip()
+				group_name = j[3].strip()
+				member = j[3].strip()
+				logging.warning("CREATE_CHAT: createGroup {} {} {}" . format(type, group_name, member))
+				result = self.create_chat(type, group_name, member = member)
+				return result
 			else:
 				return {'status': 'ERROR', 'message': '**Protocol Tidak Benar'}
 		except KeyError:
@@ -164,7 +191,60 @@ class Chat:
 			return {'status': 'ERROR', 'message': 'User Belum Login'}
 		del self.sessions[tokenid]
 		return {'status': 'OK', 'message': 'User Berhasil Logout'}
-     
+	def add_realm_chats(self, chat_id, username): # memasukkan chat_id kedalam parameter user
+		self.users[username]['chats'].append(chat_id)
+		# self.chats[chat_id]= chat_dict
+		return { 'status': 'OK', 'message': f'Berhasil menambahkan chat {chat_id} kedalam {self.realm_ip} pada user {username}' }
+	def change_self_chat(self, chat_id, chat_dict):
+		self.chats[chat_id] = chat_dict
+		return { 'status': 'OK', 'message': f'Berhasil mengubah chat chats dengan chat_id {chat_id} kedalam {self.realm_ip}' }
+
+	def create_chat(self,tokenid, type, group_name, member = None, password = None):
+		if tokenid not in self.sessions:
+			return {'status': 'ERROR', 'message': 'User Belum Login'}
+		members = []
+		user = self.sessions[tokenid]
+		username = user['username']
+		members.append(username)
+		if member is not None:
+			if self.check_user(member) == False:
+				return {'status': 'ERROR', 'message': 'User Tidak Ditemukan'}
+			members.append(member)
+		now = datetime.now()
+		current_time = now.strftime("%Y-%m-%d %H:%M:%S")
+		chat_id = str(uuid.uuid4())
+		chat_dict = {
+			'type': type,
+			'name': group_name,
+			'password': password,
+			'message': {},
+			'members': members,
+			'UpdatedAt': current_time
+		}
+		self.chats[chat_id] = chat_dict
+		user['userdetail']['chats'].append(chat_id)
+		# melakukan broadcast chat group ke semua user
+		if type == 'group':
+			for ip,val in self.realms.items():
+				string="changeSelfChat {} {} \r\n" . format(chat_id, chat_dict)
+				self.sendstring(string, ip, self.realms[ip]['port'])
+		# untuk member akan disambungkan ke realm yang bersangkutan
+		if member is not None: # jika ada member
+			if  member not in self.users.keys(): # jika member tidak dari realm ini
+				for ip,val in self.realms.items(): # mencari di realm lain
+					if member in val['users']:
+						string="addUserChat {} {} {} \r\n" . format(chat_id, member, chat_dict)
+						result = self.sendstring(string, ip, self.realms[ip]['port'])
+						if result['status']=='OK':
+							return "{}" . format(result['message'])
+						else:
+							return "Error, {}" . format(result['message'])
+						break
+			else:
+				self.users[member]['chats'].append(chat_id)
+		return { 'status': 'OK', 'message': f'Berhasil membuat chat' }
+
+		
 if __name__=="__main__":
 	j = Chat()
 	
