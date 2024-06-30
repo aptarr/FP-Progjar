64f9b616903339e8cdfacf48715848637239108e
aptarr_ <apta.rasendriya@gmail.com>
2024-07-01 00:47:31 +0700

    fix: bug path

--- pages/login.py
+++ pages/login.py
@@ -13,7 +13,7 @@ def login():
             e.page.update()
 
     logo = ft.Container(
-        content = ft.Image(src="img\logo.png", width=100, height=100),
+        content = ft.Image(src="img/logo.png", width=100, height=100),
         alignment = ft.alignment.center,
         margin = ft.margin.only(bottom=30, top=0)
     )
